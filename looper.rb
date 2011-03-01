8e3cf9ec53f13101ad3eda200b33d57da1892556
Shridatt Sugrim <ssugrim@winlab.rutgers.edu>
2011-03-01 14:06:50 -0500

    Faster pinglooper implementation

--- looper.rb
+++ looper.rb
@@ -3,24 +3,17 @@
 
 require 'logger'
 
-  log = Logger.new(STDOUT)
-  log.level = Logger::DEBUG
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
 
-  log.debug("Created logger")
-  log.info("Program started")
-  log.warn("Nothing to do!")
+nodes = [[1,1],[1,2]]
 
-  begin
-	  for x in 1..20
-		  for y in 1..20
-			 xdest = rand(19) + 1
-			 ydest = rand(19) + 1
-			 system("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@node#{x}-#{y} ifconfig exp0 192.168.#{x}.#{y} netmask 255.255.0.0")
-			 system("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@node#{x}-#{y} ping 192.168.#{xdest}.#{ydest}")
-		 end
-	 end
-  rescue => err
-    log.fatal("Caught exception; exiting")
-    log.fatal(err)
-  end
+begin
+	nodes.each do |coord|
+		system("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@node#{coord[0]}-#{coord[1]} 'nohup /usr/bin/ruby /root/node_looper.rb </dev/null >>/tmp/foo.log &'")
+	end
+rescue => err
+	log.fatal("Caught exception; exiting")
+	log.fatal(err)
+end
 
--- node_looper.rb
+++ node_looper.rb
@@ -3,20 +3,20 @@
 
 require 'logger'
 
-  log = Logger.new(STDOUT)
-  log.level = Logger::DEBUG
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
 
-  a,x,y = `hostname`.match(/node(\d*)-(\d*)./).to_a
-  puts "ifconfig exp0 192.168.#{x}.#{y} netmask 255.255.0.0"
+a,x,y = `hostname`.match(/node(\d*)-(\d*)./).to_a
+`ifconfig exp0 192.168.#{x}.#{y} netmask 255.255.0.0`
 
-#  begin
-#	  for xd in 1..20
-#		  for yd in 1..20
-#			 system("ping 192.168.#{xd}.#{yd}")
-#		 end
-#	 end
- # rescue => err
-  #  log.fatal("Caught exception; exiting")
-   # log.fatal(err)
-#  end
+begin
+	for xd in 1..20
+		for yd in 1..20
+			system("ping 192.168.#{xd}.#{yd} >> /tmp/foo.log &")
+		end
+	end
+rescue => err
+	log.fatal("Caught exception; exiting")
+	log.fatal(err)
+end
 
