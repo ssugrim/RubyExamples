f202a2d89b8d3808f9632abfa6a5533a9401317d
Shridatt Sugrim <ssugrim@winlab.rutgers.edu>
2011-03-12 03:24:57 -0500

    made threader use a map instead of a loop

--- threader.rb
+++ threader.rb
@@ -10,23 +10,21 @@ log.level = Logger::DEBUG
 begin
 	lock = Mutex.new
 	buf = Array.new
-	threads=Array.new()
-#	1.upto(5){|i| threads.push(Thread.new{5.times{slp=1+rand(9);sleep(slp);log.info("#{i} happened, slept #{slp}")}})}
-	1.upto(15) do |i|
-		threads.push( Thread.new do 
-				slp = 1 + rand(4)
-				hld = 1 + rand(9)
-				log.info("#{i} begins sleeping for #{slp}")
+	nums = (1..5).to_a
+	threads = nums.map do |i|
+		Thread.new do
+			slp = 1 + rand(4)
+			hld = 1 + rand(9)
+			log.info("#{i} begins sleeping for #{slp}")
+			sleep(slp)
+			log.info("#{i} waiting on lock") if lock.locked?
+			lock.synchronize do
+				log.info("#{i} pushes")
+				buf.push(i)
+				log.info("#{i} holding the lock for #{hld}")
 				sleep(slp)
-				log.info("#{i} waiting on lock") if lock.locked?
-				lock.synchronize do
-					log.info("#{i} pushes")
-					buf.push(i)
-					log.info("#{i} holding the lock for #{hld}")
-					sleep(slp)
-				end
+			end
 		end
-			    )
 	end
 	threads.map{|t| t.join}
 	puts buf
